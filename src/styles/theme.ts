4bd162efc7e0543f8f1f07ca2ac5759639dd8839
Chanon Roy <chanonroy@gmail.com>
2020-04-18 16:14:26 +0100

    feat: add css vars

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,12 @@ const theme = {
     black: '#384047',
     softGrey: '#999',
   },
+  shadows: {
+    z1: '0 2px 2px 0 rgba(0,0,0,.15)',
+  },
+  transition: {
+    normal: 'all 0.3s ease',
+  },
 };
 
 export default theme;
